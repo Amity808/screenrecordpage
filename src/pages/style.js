a1f6fc0ec79dc5c995796794456714254b360f0b
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-10-04 02:59:47 -0700

    fix mobile

---
+++ src/pages/style.js
@@ -0,0 +1,23 @@
+const styles = {
+    boxWidth: "xl:max-w-[1280px] w-full",
+    
+    container: "flex flex-col items-center justify-center",
+    
+    heading: "font-sora font-bold text-[40px] leading-[50.4px] text-Secondary",
+    subHeading: "font-sans font-semibold text-[28px] leading-[33.89px]",
+    paragraph: "font-work_sans font-normal text-xl leading-[30.26px] text-[#616163]",
+
+    signupBorder: "border border-Secondary h-12 rounded-xl outline-0 flex items-center gap-4 py-[3px] cursor-pointer justify-center md:justify-start",
+    formInputGroup: "flex flex-col gap-3",
+    formInput: "border border-[#B6B3C6] rounded-xl p-4 leading-[18.77px] outline-0  font-medium font-work_sans",
+    formLabel: "font-work_sans font-medium text-sm leading-[16.42px] text-Secondary",
+    formPlaceholder: "placeholder-[#626262]",
+    paddingX: "sm:px-16 px-6",
+    paddingY: "sm:py-16 py-6",
+    padding: "sm:px-16 px-10 sm:py-6 py-4",
+  
+    marginX: "sm:mx-16 mx-6",
+    marginY: "sm:my-16 my-6",
+  };
+  
+export default styles;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Secondary': '#141414',
+        'neutral-1': '#434343',
+        'neutral-2': '#E7E7ED',
+        'Primary': '#120B48',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        sora: ['var(--font-sora)'],
+        work_sans: ['var(--font-work-sans)'],
+        sen: ['var(--font-sen)'],
+      },
+    },
   },
   plugins: [],
 }
